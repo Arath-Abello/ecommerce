686da4f9744147bb3e6ac9cc40e3a3a083a086b3
arath abello <ar.abello@duocuc.cl>
2023-11-15 15:08:59 -0300

    se hicieron cambios con javascript al conteo de compra del producto al apretar el signo menos o el signo mas

---
+++ js/main.js
@@ -0,0 +1,24 @@
+let minusBtn = document.querySelector('.input__minus');
+let plusBtn = document.querySelector('.input__plus');
+let userInput  = document.querySelector('.input__number');
+
+let userInputNumber = 0;
+
+plusBtn.addEventListener('click', ()=>{
+    // cada vez que se haga click pues que sume uno 
+    userInputNumber++;
+    // accedemos al valor del input y se le asigna el nuevo valor de userInputNumber
+    userInput.value = userInputNumber;
+});
+
+minusBtn.addEventListener('click', ()=>{
+    // cada vez que se haga click pues que reste uno 
+    userInputNumber--;
+    // si el valor es menor o igual a 0
+    if(userInputNumber <= 0){
+        // entonces que no de numeros negativos y se siga manteniendo en 0 asignandole el valor nuevamente
+        userInputNumber = 0;
+    }
+        // accedemos al valor del input y se le asigna el nuevo valor de userInputNumber
+    userInput.value = userInputNumber;
+});
